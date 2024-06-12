xiaomi-sm8450-marble/android_packages_apps_Personalizations

QuickSettings: Media player mode toggle [2/2]

src/com/crdroid/settings/fragments/QuickSettings.java
@@ -42,6 +42,8 @@ import com.crdroid.settings.preferences.CustomSeekBarPreference;
 
 import lineageos.providers.LineageSettings;
 
+import com.android.internal.util.rising.SystemRestartUtils;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -59,6 +61,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String KEY_PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
     private static final String KEY_QS_UI_STYLE  = "qs_tile_ui_style";
     private static final String KEY_QS_PANEL_STYLE  = "qs_panel_style";
+    private static final String KEY_QS_COMPACT_PLAYER  = "qs_compact_media_player_mode";
 
     private ListPreference mShowBrightnessSlider;
     private ListPreference mBrightnessSliderPosition;
@@ -68,6 +71,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private ListPreference mTileAnimationInterpolator;
     private ListPreference mQsUI;
     private ListPreference mQsPanelStyle;
+    private Preference mQsCompactPlayer;
 
     private static ThemeUtils mThemeUtils;
 
@@ -115,6 +119,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mQsPanelStyle = (ListPreference) findPreference(KEY_QS_PANEL_STYLE);
         mQsPanelStyle.setOnPreferenceChangeListener(this);
+        
+        mQsCompactPlayer = (Preference) findPreference(KEY_QS_COMPACT_PLAYER);
+        mQsCompactPlayer.setOnPreferenceChangeListener(this);
 
         checkQSOverlays(mContext);
     }
@@ -146,6 +153,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                     Settings.System.QS_PANEL_STYLE, value, UserHandle.USER_CURRENT);
             updateQsPanelStyle(getActivity());
             checkQSOverlays(getActivity());
+            return true;
+        } else if (preference == mQsCompactPlayer) {
+            SystemRestartUtils.showSystemUIRestartDialog(getActivity());
             return true;
         }
         return false;

EverestOS-AOSP/packages_apps_BaseCamp

src/com/everest/basecamp/categories/QuickSettings.java
@@ -33,6 +33,7 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.everest.systemUtils;
 import com.android.internal.util.everest.ThemeUtils;
 
 import com.android.settings.R;
@@ -53,9 +54,11 @@ public class QuickSettings extends SettingsPreferenceFragment
             implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_QS_PANEL_STYLE  = "qs_panel_style";
+    private static final String KEY_QS_COMPACT_PLAYER  = "qs_compact_media_player_mode";
 
     private ListPreference mQuickPulldown;
     private ListPreference mQsPanelStyle;
+    private Preference mQsCompactPlayer;
 
     private static ThemeUtils mThemeUtils;
 
@@ -86,6 +89,9 @@ public class QuickSettings extends SettingsPreferenceFragment
         mQsPanelStyle.setValue(qsPanelStyle);
         mQsPanelStyle.setSummary(mQsPanelStyle.getEntries()[index]);
         mQsPanelStyle.setOnPreferenceChangeListener(this);
+
+        mQsCompactPlayer = (Preference) findPreference(KEY_QS_COMPACT_PLAYER);
+        mQsCompactPlayer.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -108,6 +114,9 @@ public class QuickSettings extends SettingsPreferenceFragment
             Settings.System.putIntForUser(resolver,
                     Settings.System.QS_PANEL_STYLE, value, UserHandle.USER_CURRENT);
             updateQsPanelStyle(getActivity());
+            return true;
+        } else if (preference == mQsCompactPlayer) {
+            systemUtils.showSystemUIRestartDialog(getActivity());
             return true;
         }
         return false;